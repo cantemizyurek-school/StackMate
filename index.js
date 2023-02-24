473ef688a2601dc33a336e5481ff4874bb737d56
Can_Temizyurek_School <jhonnytemizyurek@gmail.com>
2023-02-24 02:06:45 -0500

    Got File Ready For NPM

---
+++ index.js
@@ -0,0 +1,4 @@
+import SinglyLinkedList from "./src/SinglyLinkedList.js";
+import DoublyLinkedList from "./src/DoublyLinkedList.js";
+
+export { SinglyLinkedList, DoublyLinkedList };
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import SinglyLinkedList from "./SinglyLinkedList";
-import DoublyLinkedList from "./DoublyLinkedList";
-
-export default { SinglyLinkedList, DoublyLinkedList };
