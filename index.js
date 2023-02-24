2d4ead6b61d1dd5820355f9b1072e09b9297bc80
Can_Temizyurek_School <jhonnytemizyurek@gmail.com>
2023-02-24 11:02:02 -0500

    Add BinaryMinHeap

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ import DoublyLinkedList from "./src/DoublyLinkedList.js";
 import Queue from "./src/Queue.js";
 import Stack from "./src/Stack.js";
 import BinaryHeap from "./src/BinaryHeap.js";
+import BinaryMinHeap from "./src/BinaryMinHeap.js";
 
-export { SinglyLinkedList, DoublyLinkedList, Queue, Stack, BinaryHeap };
+export {
+  SinglyLinkedList,
+  DoublyLinkedList,
+  Queue,
+  Stack,
+  BinaryHeap,
+  BinaryMinHeap,
+};
