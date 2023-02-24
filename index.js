0e1d9cc17076c0f6979d0e0855251967f91b2faf
Can_Temizyurek_School <jhonnytemizyurek@gmail.com>
2023-02-24 08:56:50 -0500

    Add Queue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import SinglyLinkedList from "./src/SinglyLinkedList.js";
 import DoublyLinkedList from "./src/DoublyLinkedList.js";
+import Queue from "./src/Queue.js";
 
 export { SinglyLinkedList, DoublyLinkedList };
---
+++ src/Queue.js
@@ -0,0 +1,48 @@
+import SinglyLinkedList from "./SinglyLinkedList.js";
+
+export default function Queue() {
+  var list = SinglyLinkedList();
+
+  return {
+    enqueue,
+    dequeue,
+    peek,
+    length,
+    isEmpty,
+    clear,
+    toArray,
+    toString,
+  };
+
+  function enqueue(data) {
+    list.push(data);
+  }
+
+  function dequeue() {
+    return list.shift();
+  }
+
+  function peek() {
+    return list.head();
+  }
+
+  function length() {
+    return list.length();
+  }
+
+  function isEmpty() {
+    return length() === 0;
+  }
+
+  function clear() {
+    list = new SinglyLinkedList();
+  }
+
+  function toArray() {
+    return list.toArray();
+  }
+
+  function toString() {
+    return list.toString();
+  }
+}
